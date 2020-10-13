20ac925a742edeedd9303bf1373d7528eed7512a
JathurshanG <jathur93@gmail.com>
2020-10-13 22:26:41 +0200

    Essaie Markdown

--- Script.R
+++ Script.R
@@ -1,32 +1,21 @@
-###########################################################################
-# Projet Portant sur la création d'un DashBoard ---------------------------
-###########################################################################
-#setwd
-getwd()
-setwd("C:/Users/jathu/Desktop/M2-sep/TSASR/Tennis")
-
-# Library / Require -------------------------------------------------------
+# Library / Require -----------------------------------------------------------------------------------
 
 library(tidyverse)
-library(dplyr)
-library(stats)
-library(readr)
 library(lubridate)
-library(purrr)
-library(stringr)
 library(data.table)
 
-# Exportation des données -------------------------------------------------
+# Exportation des données -----------------------------------------------------------------------------
 #Exporter les bases de données nécessaire via github
 #git clone  https://github.com/JeffSackmann/tennis_atp
 
 
-# Exportation de la base joueur en question ---------------------------------------------------
+# Importation de la base joueur -----------------------------------------------------------------------
 player <- read_csv("tennis_atp/atp_players.csv",col_names = FALSE, locale = locale(),na="empty")
 head(player)
 colnames(player) <- c("id","Prenom","Nom","Hand","Birth","Nat")
+player$Birth=ymd(player$Birth)
 
-#la variable date contient des valeurs manquante.
+#la variable date contient des valeurs manquantes.
 #a=which(is.na(player$Birth))
 #na_birth=player[a,]
 #11582/54938
@@ -34,41 +23,41 @@ colnames(player) <- c("id","Prenom","Nom","Hand","Birth","Nat")
 ##warning message :  125 failed to parse
 
 
-# Exportation des Résultat de match (simple) ------------------------------
-lst <- list.files(path = "C:/Users/jathu/Desktop/M2-sep/TSASR/Tennis/tennis_atp")
+# Importation des résultats de match (simple) ---------------------------------------------------------
+lst <- list.files(path = "tennis_atp")
 lst_data <- grep(pattern = "^atp_matches_[[:digit:]]{4}.csv$", x = lst, value = TRUE)
 lst_names <- paste('atp', str_extract(string = lst_data, pattern = "[[:digit:]]{4}"), sep = "")
-lst_tib <- map(.x = lst_data, function (x) read_csv(paste("", x, sep = "C:/Users/jathu/Desktop/M2-sep/TSASR/Tennis/tennis_atp/")))
+lst_tib <- map(.x = lst_data, function (x) read_csv(paste("tennis_atp/", x, sep = "")))
 names(lst_tib) <- lst_names
 atp <- reduce(.x = lst_tib, .f = bind_rows)
+head(atp)
 
 
-# Exporter base double ----------------------------------------------------
-lst <- list.files(path = "C:/Users/jathu/Desktop/M2-sep/TSASR/Tennis/tennis_atp")
+# Importation des résultats de match (double) ---------------------------------------------------------
+lst <- list.files(path = "tennis_atp")
 lst_data <- grep(pattern = "^atp_matches_doubles_[[:digit:]]{4}.csv$", x = lst, value = TRUE)
 lst_names <- paste('atp_double', str_extract(string = lst_data, pattern = "[[:digit:]]{4}"), sep = "")
-lst_tib <- map(.x = lst_data, function (x) read_csv(paste("", x, sep = "C:/Users/jathu/Desktop/M2-sep/TSASR/Tennis/tennis_atp/")))
+lst_tib <- map(.x = lst_data, function (x) read_csv(paste("tennis_atp/", x, sep = "")))
 names(lst_tib) <- lst_names
 atp_double <- reduce(.x = lst_tib, .f = bind_rows)
+head(atp_double)
 
 
-
-# Exporter base Futures ---------------------------------------------------
-lst <- list.files(path = "C:/Users/jathu/Desktop/M2-sep/TSASR/Tennis/tennis_atp")
+# Importation base futures ----------------------------------------------------------------------------
+lst <- list.files(path = "tennis_atp")
 lst_data <- grep(pattern = "^atp_matches_futures_[[:digit:]]{4}.csv$", x = lst, value = TRUE)
 lst_names <- paste('atp_futures', str_extract(string = lst_data, pattern = "[[:digit:]]{4}"), sep = "")
-lst_tib <- map(.x = lst_data, function (x) read_csv(paste("", x, sep = "C:/Users/jathu/Desktop/M2-sep/TSASR/Tennis/tennis_atp/"),col_types = cols(tourney_level = col_character())))
-#Après un premier essaie à l'imporatation la donnée de 2020, nous posent un problèmes,
-#en effet à des variables, tourney_level -> en 2020, a des variables numeriques. 
+lst_tib <- map(.x = lst_data, function (x) read_csv(paste("tennis_atp/", x, sep = ""),col_types = cols(tourney_level = col_character())))
 names(lst_tib) <- lst_names
 atp_futures <- reduce(.x = lst_tib, .f = bind_rows)
+head(atp_futures)
 
 
-# Exporter Base Quall_chall -----------------------------------------------
-lst <- list.files(path = "C:/Users/jathu/Desktop/M2-sep/TSASR/Tennis/tennis_atp")
+# Importation base qual_chall -------------------------------------------------------------------------
+lst <- list.files(path = "tennis_atp")
 lst_data <- grep(pattern = "^atp_matches_qual_chall_[[:digit:]]{4}.csv$", x = lst, value = TRUE)
 lst_names <- paste('atp_qual_chall_', str_extract(string = lst_data, pattern = "[[:digit:]]{4}"), sep = "")
-lst_tib <- map(.x = lst_data, function (x) read_csv(paste("", x, sep = "C:/Users/jathu/Desktop/M2-sep/TSASR/Tennis/tennis_atp/"),
+lst_tib <- map(.x = lst_data, function (x) read_csv(paste("tennis_atp/", x, sep = ""),
                                                     col_types = cols(best_of = col_double(),draw_size = col_double(), 
                                                                      loser_age = col_double(),loser_ht = col_double(), 
                                                                      loser_id = col_double(),  loser_name = col_character(), 
@@ -84,25 +73,25 @@ lst_tib <- map(.x = lst_data, function (x) read_csv(paste("", x, sep = "C:/Users
 #les variables présentent des WC dans la base de données, a ce titre, j'ai décider de configurer chacune des variable,
 #en décidant de leur symboles
 names(lst_tib) <- lst_names
-atp_quall_chall <- reduce(.x = lst_tib, .f = bind_rows)
+atp_qual_chall <- reduce(.x = lst_tib, .f = bind_rows)
+head(atp_qual_chall)
 
 
-# Exporter base Claseement ----------------------------------------------------
-lst <- list.files(path = "C:/Users/jathu/Desktop/M2-sep/TSASR/Tennis/tennis_atp")
+# Importation base classement -------------------------------------------------------------------------
+lst <- list.files(path = "tennis_atp")
 lst_data <- grep(pattern = "^atp_rankings_[[:digit:]]{2}s.csv$", x = lst, value = TRUE)
-lst_names <- paste('atp_Classement_', str_extract(string = lst_data, pattern = "[[:digit:]]{2}"), sep = "") 
-lst_tib <- map(.x = lst_data, function (x) read_csv(paste("", x, sep = "C:/Users/jathu/Desktop/M2-sep/TSASR/Tennis/tennis_atp/")))
+lst_names <- paste('atp_rankings_', str_extract(string = lst_data, pattern = "[[:digit:]]{2}"), sep = "") 
+lst_tib <- map(.x = lst_data, function (x) read_csv(paste("tennis_atp/", x, sep = "")))
 names(lst_tib) <- lst_names
 atp_classement <- reduce(.x = lst_tib, .f = bind_rows)
-#exportation de la base Actuelle.
+#Importation de la base actuelle.
 atp_rankings_current <- read_csv("tennis_atp/atp_rankings_current.csv",
                                  col_names = FALSE, locale = locale(date_names = "fr"))
-colnames(atp_classement)->colnames(atp_rankings_current)
-
+colnames(atp_rankings_current) <- colnames(atp_classement)
 
 atp_classement %>%
   bind_rows(atp_rankings_current) %>%
   mutate(ranking_date = ymd(ranking_date))->atp_classement
 #verif Ok
 max(atp_classement$ranking_date)
-#la valeuer max est le 30 dec 2019, la totalité des variables a été ajouté
+#la valeur max est le 09 mars 2020, la totalité des variables a été ajouté
