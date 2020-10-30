10925037c9dbc3709540234b5f17e11bce354c68
Alicia Ehre <alicia.ehre@gmail.com>
2020-10-30 20:28:47 +0100

    Réorganisation du script + Automatisation + Modification de certains graphs + Ajout de graphs

--- Script.R
+++ Script.R
@@ -1,11 +1,18 @@
- # Projet Portant sur la création d'un DashBoard ---------------------------
+ # Projet portant sur la création d'un DashBoard -------------------------------------------------
+ 
 
- # 0 - Packages ------------------------------------------------------------
+ # 0 - Préparation de l'environnement de travail -------------------------------------------------
+ #Importation des données via le script Export.R
+ #Base de données nécessaires pour ce script: player, atp, atp_classement
+ #Vider la mémoire
+ rm(list=c('lst','lst_data','lst_names',"lst_tib"))
+ 
+ # 1 - Packages ----------------------------------------------------------------------------------
  library(tidyverse)
  library(fmsb)
- library(DT)
+ library(data.table)
  
- # 1 - Performances d'un joueur -------------------------------------------
+ # 2 - Récupération des données du joueur selectionné --------------------------------------------
  
  #Automatisation du code pour qu'il s'adapte à tous les joueurs
  Firstname <- 'Juan Martin'
@@ -21,13 +28,16 @@
  atp %>%
    filter(winner_id == id_joueur | loser_id == id_joueur) -> joueur  #620 matchs en tout
  
+ # 3 - Préparation des jeux de données utilisés pour les graphs ----------------------------------
+ 
  #Matchs par surface
  joueur %>%
    mutate(surface = factor(surface, levels = names(sort(table(surface))), ordered = TRUE)) %>%
    group_by(surface) %>%
    summarize(N = n()) %>%
    rename(Surface = surface) %>%
-   mutate(Surface = case_when(Surface == "Clay" ~ "Terre battue",
+   mutate(Surface = case_when(Surface == "Carpet" ~ "Tapis",
+                              Surface == "Clay" ~ "Terre battue",
                               Surface == "Grass" ~ "Herbe",
                               Surface == "Hard" ~ "Dur")) -> surfaces
  
@@ -63,25 +73,94 @@
    filter(!is.na(tourney_result)) %>%
    distinct() %>%
    arrange(tourney_date)-> tournoi_joueur
-
- View(tournoi_joueur)
  
- # 2 - Graphiques des competitions -----------------------------------------
+ # 4 - Identité et caractéristiques du joueur --------------------------------------------------
+ 
+ #Affichage de l'identité
+ 
+ player %>%
+   mutate(Hand=case_when(Hand=="R"~"Droitier",
+                         Hand=="L"~"Gaucher",
+                         Hand=='A'~"Ambidextre",
+                         Hand=="U"~"Inconnu")) -> a
+ 
+ a <- a[which(player$id==id_joueur),]
+ 
+ b <- fread("https://sql.sh/ressources/sql-pays/sql-pays.csv",encoding = 'UTF-8')
+ b <- b[which(b$V4==a$Nat),5]
+ 
+ Nom <- a$Prenom
+ Prenom <- a$Nom
+ DTN <- format(a$Birth,"%d %B %Y")
+ Age <- year(today())-year(a$Birth)
+ Main <- a$Hand
+ Nationalite <- b$V5
+ 
+ a <- (paste("Le joueur selectionné est",a$Nom, a$Prenom,
+             ", né le",format(a$Birth,"%d %B %Y"),".","Il est",a$Hand,"et il vient de",b$V5))
+ a
+ rm(list=c("a","b"))
+ 
+ # 5 - Graphiques sur les surfaces de jeu --------------------------------------------------------
  
  #Repartition des matchs joues en fonction de la surface, sur toute sa carriere
  surfaces %>%
-   drop_na() %>% #NA supprimé
    mutate(textpos = cumsum(N) - N/2) %>%
    ggplot(mapping = aes(x = 1, y = N, fill = Surface)) +
    geom_col(position = 'stack', width = 1) +
    coord_polar(theta = 'y', start = 0) +
-   scale_fill_discrete(type = c('Terre battue' = 'chocolate', 'Herbe' = 'chartreuse4', 'Dur' = "deepskyblue4")) + 
+   scale_fill_discrete(type = c('Terre battue' = 'chocolate', 'Herbe' = 'chartreuse4', 'Dur' = "deepskyblue4", 'Tapis' = "grey")) + 
    theme_void() +
    geom_text(aes(y = textpos, 
                  label = paste(N, ' (', round(N/sum(N)*100,1), '%' ,')', sep = '')),
                  size=5, 
-                 position = position_stack(vjust=0.5)) +
+                 position = position_stack(vjust=0.6)) +
    ggtitle("Nombre de matchs joués en fonction du terrain.")
+ 
+ #Radarchart du pourcentage de matchs gagné par sufaces
+ # On récupère les matchs gagnés en fonction de la surface
+ joueur %>%
+   filter(winner_id == id_joueur) %>%
+   group_by(surface) %>%
+   summarize (G = n()) %>% #Nb matchs gagnés
+   rename(Surface = surface) %>%
+   mutate(Surface = case_when(Surface == "Carpet" ~ "Tapis",
+                              Surface == "Clay" ~ "Terre battue",
+                              Surface == "Grass" ~ "Herbe",
+                              Surface == "Hard" ~ "Dur")) -> matchs_gagnes
+ 
+ match_joue_gagne <- merge(surfaces,matchs_gagnes, by = "Surface")
+ match_joue_gagne %>%
+   mutate(max = 1, 
+          min = 0, 
+          rapport = G/N) %>%
+   select('Surface','max','min','rapport') -> radar2
+ 
+ #barplot
+ radar2 %>%
+   ggplot(mapping = aes(x = Surface, y=rapport*100, fill = Surface)) +
+   geom_col() +
+   scale_fill_discrete(type = c('Terre battue' = 'chocolate', 'Herbe' = 'chartreuse4', 'Dur' = "deepskyblue4", 'Tapis' = "grey")) + 
+   labs(y = "Matchs gagnés", x = "Surface") +
+   ggtitle("Pourcentage de matchs gagnés par surface")
+
+ #radarchart
+ radar2 <- transpose(radar2)
+ colnames(radar2) <- radar2[1,]
+ radar2 <- radar2[-1,]
+ radar2 <-map_dfr(radar2, as.numeric)
+ 
+ radarchart(radar2,axistype=1 ,
+            seg = 10,
+            title = "Pourcentage de matchs gagnés par surface",
+            #custom polygon
+            pcol=rgb(0.2,0.5,0.5,0.9) , pfcol=rgb(0.2,0.5,0.5,0.5) , plwd=2 , 
+            #custom the grid
+            cglcol="grey", cglty=1, axislabcol="grey", caxislabels=seq(0,100,10), cglwd=0.8,
+            #custom labels
+            vlcex=0.8 )
+ 
+ # 6 - Résultats en fonction des adversaires --------------------------------------------------------
  
  #Nombre de matchs gagnes et perdus en focntion des adversaires au cours de sa carriere
  bilan_opp_reduit <- bilan_opp[1:30,] #sélection es 30 joueurs qu'il a le plus affronté, sinon le graphique était illisible
@@ -97,7 +176,24 @@
    geom_text(mapping = aes(y = -0.1, label = Total), size = 2) +
    labs(fill = "Résultat")
  
- #Grand chelem
+ #Comparaison avec le top 3 mondial (A AUTOMATISER)
+ radar1 <- data.frame(
+   "Rafael" = c(1,0,0.33),
+   "Federer" = c(1,0,0.28),
+   "Djokovic" = c(1,0,0.2))
+ 
+ radarchart(radar1,axistype=1 , 
+            #custom polygon
+            pcol=rgb(0.2,0.5,0.5,0.9) , pfcol=rgb(0.2,0.5,0.5,0.5) , plwd=4 , 
+            #custom the grid
+            cglcol="grey", cglty=1, axislabcol="grey", caxislabels=seq(0,20,5), cglwd=0.8,
+            #custom labels
+            vlcex=0.8 ) +
+   title("Résultats face au Top3")
+ 
+ # 7 - Résultats aux grands chelems --------------------------------------------------------
+ 
+ #Nombre de matchs joués pargrand chelem
  tournoi_joueur %>%
     filter(tourney_name %in% c('Roland Garros', 'Australian Open', 'Wimbledon', 'US Open')) %>%
     group_by(tourney_name) %>%
@@ -114,7 +210,6 @@
     ggtitle("Tournois du grand chelem joués.") +
     labs(fill = "Nom du tournoi")
  
-    
  
  #Résultat par tournoi Grand chelem
  joueur %>%
@@ -134,15 +229,118 @@
     ggtitle("Matchs gagnés et matchs perdus par tournoi du grand chelem") +
     labs(fill = "Résultat")
  
+ #Résultats des grands chelems par année
+ rm(annee)
+ joueur %>%
+   filter(tourney_name %in% c('Roland Garros', 'Australian Open', 'Wimbledon', 'US Open')) %>%
+   mutate(year = str_sub(tourney_id, 1, 4),
+          result =case_when(winner_id == id_joueur ~ 'Gagné',
+                            winner_id != id_joueur ~ 'Perdu')) %>%
+   select('tourney_name','tourney_id','year','result','winner_id','loser_id') -> annee
  
+ View(annee)
+ 
+ annee %>%
+   group_by(year, tourney_name, result) %>%
+   summarise(N=n()) %>%
+   arrange(desc(N)) %>%
+   mutate(y2=round((N/sum(N))*100), ypos= cumsum(N)-N/2, labels= paste(N,paste0("(",y2,"%)")) )  %>%
+   ggplot(mapping=aes(x=year, y=N, fill=result))+
+   geom_col()+
+   scale_fill_discrete(type = c('Perdu' = "tomato3", "Gagné" = "springgreen3")) +
+   facet_wrap(facets = "tourney_name", ncol  = 2)+
+   theme_bw() +
+   labs(y = "Nombre de confrontations", x = 'year') +
+   geom_text(mapping = aes(y = ypos, label = N), size = 4) +
+   labs(fill = "Résultat")
+ 
+ 
+ #Résultats par année en fonction des grands chelems
+ annee %>%
+   mutate(tourney_abv=str_sub(tourney_name,1,3))%>%
+   group_by(year, tourney_abv,result) %>%
+   summarise(N=n()) %>%
+   arrange(desc(N)) %>%
+   mutate(y2=round((N/sum(N))*100), ypos= cumsum(N)-N/2, labels= paste(N,paste0("(",y2,"%)")) )  %>%
+   ggplot(mapping=aes(x=tourney_abv, y=N, fill=result))+
+   geom_col()+
+   scale_fill_discrete(type = c('Perdu' = "tomato3", "Gagné" = "springgreen3")) +
+   facet_wrap(facets = "year", ncol  = 4)+
+   theme_bw() +
+   labs(y = "Nombre de confrontations", x = 'year') +
+   geom_text(mapping = aes(y = ypos, label = N), size = 3) +
+   labs(fill = "Résultat")
+ 
+ 
+ # 8 - Points gagnés par années et tournois -----------------------------------------
+ 
+ #Récupération de la date du premier tournoi
+ atp %>%
+   filter(winner_id==id_joueur | loser_id==id_joueur) %>%
+   summarise(tourney_date)%>%
+   min() %>%
+   ymd() -> b
+ 
+ #Parcours en carrière professionnelle
+ #Points gagnés
+ atp_classement %>%
+   filter(player==id_joueur) %>%
+   ggplot(aes(x=ranking_date,y=points))+
+   geom_line()+
+   theme_grey()+
+   xlab("Date")+
+   ylab("Points Marqués")+
+   ggtitle(paste('Points marqués par',Lastname,'durant sa carrière professionnelle.',sep=" ")) -> Graph_point
+ Graph_point
+ 
+ #partie suivante à retravailler pour voir si on peut sortir des graphs intéressants
+ 
+ #ses meilleurs années (celles où il est dans top 5) 
+ atp_classement %>%
+   filter(rank <=5 & player == id_joueur)-> best_annee #on a ses matchs où il gagnait et etait dans le top10 
+unique(best_annee$ranking_date)
+#OU, c'est peut-être plus logique d'utiliser la table atp_classement
+ joueur%>%
+   filter(winner_rank <= 5 & winner_id == id_joueur)-> annee_meil #on a ses matchs où il gagnait et etait dans le top10 
+ view(annee_meil) 
+ str(annee_meil$tourney_id)
+ unique(annee_meil$tourney_id)
+ #les dates sont 2009;2010;2013;2014;2018;2019
+ 
+ #Voir quand il a perdu contre un joueur du top 5
+ joueur%>%
+   filter(loser_id == id_joueur & winner_rank <= 5)-> los5
+ View(los5)
+ 
+ #Nombre de points par tournoi gagné
+ #roland garros rg
+ joueur%>%
+   filter(tourney_name == "Roland Garros" & winner_id == id_joueur) ->rga
+ view(rga$winner_rank_points)
+ liste<-rga$winner_rank_points #on peut faire la moyenne
+ moy<-mean(liste) #3348,32 points en moy au roland garos
+ #autralie open
+ joueur%>%
+   filter(tourney_name == "Australian Open" & winner_id == id_joueur) ->aop
+ moy_aop<-mean(aop$winner_rank_points) #3495 points en moy en Open Aust
+ #Wimbledon
+ joueur%>%
+   filter(tourney_name == "Wimbledon" & winner_id == id_joueur) ->wbn
+ moy_wbn<-mean(wbn$winner_rank_points) #2996,28 points en moy en winbledon
+ #Us open
+ joueur%>%
+   filter(tourney_name == "US Open" & winner_id == id_joueur) ->uso
+ moy_uso<-mean(uso$winner_rank_points)#3102,77 points en moy en US open
+ 
+ 
+ # 9 - Classement du joueur --------------------------------------------------------
  
  #Evolution au classement ATP depuis les debuts en professionnel
+
  atp_classement %>%
-    filter(player == 105223) -> classement_delpo   #classement hebdomadaire
+    filter(player == id_joueur) -> classement_joueur #classement hebdomadaire
  
- classement_delpo <- classement_delpo[-c(1:31),]   #on ne garde qu'a partir de 2005
- 
- ggplot(classement_delpo, aes(x = ranking_date, y = rank)) + 
+ ggplot(classement_joueur, aes(x = ranking_date, y = rank)) + 
     labs(title = "Evolution du classement ATP de Del Potro depuis 2005", x = "Années", y = "Classement") +
     geom_line(col = "blue") + 
     scale_y_reverse() +
@@ -150,157 +348,4 @@
 
 
 
-# 3 - Identité et parcours du joueur --------------------------------------------------
-
-rm(list=c('lst','lst_data','lst_names',"lst_tib"))
- 
-#Affichage de l'identité
-
-player %>%
-  mutate(Hand=case_when(Hand=="R"~"Droitier",
-                        Hand=="L"~"Gaucher",
-                        Hand=='A'~"Ambidextre",
-                        Hand=="U"~"Inconnu"))->a
-
-a<-a[which(player$id==id_joueur),]
-
-b=fread("https://sql.sh/ressources/sql-pays/sql-pays.csv",encoding = 'UTF-8')
-b<- b[which(b$V4==a$Nat),5]
-
-Nom<-a$Prenom
-Prenom<-a$Nom
-DTN <- format(a$Birth,"%d %B %Y")
-Age=year(today())-year(a$Birth)
-Main <- a$Hand
-Nationalite<-b$V5
-
-a<-(paste("Le joueur selectionné est ",a$Nom, a$Prenom,
-          ", né le ",format(a$Birth,"%d %B %Y"),".","Il est ",a$Hand,".","et il vient de ",b$V5))
-a
-rm(list=c("a","b"))
-
-
-#Analyse du premier match !
-atp %>%
-  filter(winner_id==id_joueur | loser_id==id_joueur) %>%
-  summarise(tourney_date)%>%
-  min() %>%
-  ymd() -> b
-
-
-#Parcours en carrière professionnelle
-#Points gagnés
-atp_classement %>%
-  filter(player==id_joueur) %>%
-  ggplot(aes(x=ranking_date,y=points))+
-  geom_line()+
-  theme_grey()+
-  xlab("Date")+
-  ylab("Points Marqués")+
-  ggtitle(paste('Graphique représentant les points marqués par',Lastname,sep=" ")) -> Graph_point
-Graph_point
-
-
-
-#Graphique utile
-potro_radar <- data.frame(
-  "Rafael" = c(1,0,0.33),
-  "Federer" = c(1,0,0.28),
-  "Djokovic" = c(1,0,0.2)
-)
-
-radarchart(potro_radar,axistype=1 , 
-           
-           #custom polygon
-           pcol=rgb(0.2,0.5,0.5,0.9) , pfcol=rgb(0.2,0.5,0.5,0.5) , plwd=4 , 
-           
-           #custom the grid
-           cglcol="grey", cglty=1, axislabcol="grey", caxislabels=seq(0,20,5), cglwd=0.8,
-           
-           #custom labels
-           vlcex=0.8 
-)
-title("Résultats face au Top3")
-
-
-#on trie les matchs par surfaces
-battue<-filter(joueur, surface == "Clay")
-nrow(battue) #121 matchs sur terre battue
-battue<-filter(joueur, surface == "Clay" & winner_id == 105223)  #84 victoires
-nrow(battue)
-
-
-tapis<-filter(joueur, surface == "Carpet")
-nrow(tapis)#8 matchs sur tapis
-
-tapis<-filter(joueur, surface == "Capert" & winner_id == 105223) #6 victoires
-nrow(tapis)
-
-
-dur<-filter(joueur, surface == "Hard")
-view(dur)
-nrow(dur) #433 matchs sur dur
-dur<-filter(joueur, surface == "Hard" & winner_id == 105223) #313 victoires
-nrow(dur)
-
-herbe<-filter(joueur, surface == "Grass")
-nrow(herbe) #58 macths sur herbe
-herbe<-filter(joueur, surface == "Grass" & winner_id ==105223)#40 victoires
-nrow(herbe)
-
-del_radar <- data.frame(     #1 et 0 sont les bornes max et min , 1 et 0 parce qu'on
-  "Herbe" = c(1,0,40/58),   # a fait le nbre de victoire sur le nbre total de match
-  "Dur" = c(1,0,313/433),   # on pouvait multiplier par 100 
-  "Terre battue" = c(1,0,84/121),
-  "Tapis"= c(1,0,6/8)
-)
-view(del_radar)
-
-radarchart(del_radar,axistype=1 , 
-           
-           #custom polygon
-           pcol=rgb(0.2,0.5,0.5,0.9) , pfcol=rgb(0.2,0.5,0.5,0.5) , plwd=4 , 
-           
-           #custom the grid
-           cglcol="grey", cglty=1, axislabcol="grey", caxislabels=seq(0,20,5), cglwd=0.8,
-           
-           #custom labels
-           vlcex=0.8 
-)
-title("Caractéristiques du joueur")
-
-View(joueur)
-#ses meilleurs années, on suppose que ses meilleures années sont celles 
-#où il est dans top 5 
-joueur%>%
-  filter(winner_rank <= 5 & winner_id == 105223)-> annee_meil #on a ses matchs où il gagnait et etait dans le top10 
-view(annee_meil) 
-str(annee_meil$tourney_id)
-unique(annee_meil$tourney_id)
-#voir la 1e colonne, les dates sont 2009;2010;2013;2014;2018;2019
-joueur%>%
-  filter(loser_id == 105223 & winner_rank <= 5)-> los5
-
-#Nombre de points par tournoi gagné
-#roland garros rg
-joueur%>%
-  filter(tourney_name == "Roland Garros" & winner_id == 105223) ->rga
-view(rga$winner_rank_points)
-liste<-rga$winner_rank_points #on peut faire la moyenne
-moy<-mean(liste) #3348,32 points en moy au roland garos
-
-#autralie open
-joueur%>%
-  filter(tourney_name == "Australian Open" & winner_id == 105223) ->aop
-moy_aop<-mean(aop$winner_rank_points) #3495 points en moy en Open Aust
-
-#Wimbledon
-joueur%>%
-  filter(tourney_name == "Wimbledon" & winner_id == 105223) ->wbn
-moy_wbn<-mean(wbn$winner_rank_points) #2996,28 points en moy en winbledon
-
-#Us open
-joueur%>%
-  filter(tourney_name == "US Open" & winner_id == 105223) ->uso
-moy_uso<-mean(uso$winner_rank_points)#3102,77 points en moy en US open
 
